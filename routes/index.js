817af476740d36fb803c04d58b0ed909a8c2cf6b
YOUR FULL NAME <morris.jess26@gmail.com>
2016-07-06 10:08:05 -0600

    added new partials and a New Group form ejs

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
     res.render('index', {
         title: 'Express'
     });
@@ -13,9 +12,12 @@ router.get('/', function(req, res, next) {
 
 //dummy route
 router.get('/loggedin', function(req, res, next) {
-
   res.render('loggedin');
 });
 
+//dummy route
+router.get('/newgroup', function(req, res, next) {
+  res.render('newgroup');
+});
 
 module.exports = router;
