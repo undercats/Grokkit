d3f5470e38c3e47e9a274b2bc044ba947c9b6d74
gabethexton <gabethexton@gmail.com>
2016-07-07 11:16:43 -0600

    working on bad request error

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "oauth/google/callback"
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     function(accessToken, refreshToken, profile, cb) {
         console.log('\nINCOMING PROFILE IS:\n', profile, '\nEND INCOMING PROFILE\n');
