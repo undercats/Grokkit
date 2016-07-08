c6b3eab8debe5d56fa6e729f112bee1b60d326ba
gabethexton <gabethexton@gmail.com>
2016-07-08 10:26:32 -0600

    fixing auth route 13

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ var checkit = require('checkit');
 router.get('/:username', function(req, res, next) {
 
     console.log('req.params =\n', req.params);
-    console.log('req.session =\n', req.session.passport.user);
+    // console.log('req.session =\n', req.session.passport.user);
     console.log(req.session.passport.user.username);
     if (req.session[0].user.username === req.params[0].username) {
     var newData = [];
